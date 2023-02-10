e198af1caf6c90d0e87e87756bbd1aba10c91216
M.R <m.r.MeijoUniv@gmail.com>
2023-02-10 23:04:00 +0900

    追加の音符を追加

--- TeamDevelopmentARTest/Assets/Scripts/ObjectChangeScale.cs
+++ TeamDevelopmentARTest/Assets/Scripts/ObjectChangeScale.cs
@@ -5,9 +5,25 @@ using DG.Tweening;
 
 public class ObjectChangeScale : MonoBehaviour
 {
+    private float scaleValue = 1.0f;
     private void Start()
     {
-        transform.DOScale(new Vector3(2, 2, 2), 2.0f).SetLoops(-1, LoopType.Yoyo);
+        string objName = this.gameObject.name;
+        switch(objName)
+        {
+            case "OnpuText":
+                scaleValue = 0.5f;
+                break;
+            case "Onpu2Text":
+                scaleValue = 2.0f;
+                break;
+            default:
+                Debug.LogError("想定されている名前でないオブジェクトがあります");
+                break;
+        }
+
+        transform.DOScale(new Vector3(scaleValue, scaleValue, scaleValue), 2.0f).SetLoops(-1, LoopType.Yoyo);
+        
     }
     
 }
